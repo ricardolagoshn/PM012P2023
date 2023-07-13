dd33e8fd0f873d5b2c3b5162679b78c35d3b1651
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-07-12 20:44:01 -0600

    Cambios al 12/07/2023

--- app/src/main/java/com/example/pm012p2023/ActivityEmpleCreate.java
+++ app/src/main/java/com/example/pm012p2023/ActivityEmpleCreate.java
@@ -1,8 +1,26 @@
 package com.example.pm012p2023;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.android.volley.*;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -12,15 +30,134 @@ import com.example.pm012p2023.Config.RestApiMethods;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ActivityEmpleCreate extends AppCompatActivity {
 
+    static final  int REQUEST_IMAGE = 101;
+    static final  int PETICION_ACCESS_CAM = 201;
+
+    ImageView imageView;
+    Button btntakefoto, btnguardar;
+    String currentPhotoPath;
     private RequestQueue requestQeue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emple_create);
 
-        sendData();
+        imageView = (ImageView) findViewById(R.id.imageViewFoto);
+        btntakefoto = (Button) findViewById(R.id.btnfoto);
+        btnguardar = (Button) findViewById(R.id.btnguardar);
+
+        btntakefoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                permisos();
+            }
+        });
+
+        btnguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData();
+            }
+        });
+    }
+
+    private void permisos()
+    {
+        // Metodo para obtener los permisos requeridos de la aplicacion
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA},PETICION_ACCESS_CAM);
+        }
+        else
+        {
+            dispatchTakePictureIntent();
+            //TomarFoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == PETICION_ACCESS_CAM)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                dispatchTakePictureIntent();
+                //TomarFoto();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "se necesita el permiso de la camara",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_IMAGE)
+        {
+            //Bundle extra = data.getExtras();
+            //Bitmap imagen = (Bitmap) extra.get("data");
+            //imageView.setImageBitmap(imagen);
+
+            try {
+                File foto = new File(currentPhotoPath);
+                imageView.setImageURI(Uri.fromFile(foto));
+            }
+            catch (Exception ex)
+            {
+                ex.toString();
+            }
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.toString();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.pm012p2023.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void sendData()
@@ -34,7 +171,7 @@ public class ActivityEmpleCreate extends AppCompatActivity {
         emple.setFechanac("1987-01-01");
         emple.setGenero("M");
         emple.setTelefono("99665534");
-        emple.setFoto("sdsdsdsdsdsdsdsdsdsdsdsdsdsdsds");
+        emple.setFoto(ConvertImage64(currentPhotoPath));
 
         JSONObject jsonemple = new JSONObject();
 
@@ -74,5 +211,15 @@ public class ActivityEmpleCreate extends AppCompatActivity {
         });
 
         requestQeue.add(resquest);
+    }
+
+
+    private String ConvertImage64(String path)
+    {
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] imagearray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imagearray, Base64.DEFAULT);
     }
 }
